38c68f8158fab3bd1449deebc0e4749de2f6c59a
Alexpux <alexey.pawlow@gmail.com>
2013-11-10 10:12:01 +0400

    Fix regen-info script

--- msys2-base/regen-info.sh
+++ msys2-base/regen-info.sh
@@ -3,7 +3,7 @@
 INFODIR=/usr/share/info
 rm -f $INFODIR/dir
 
-for F in $INFODIR/*.info; do
+for F in $INFODIR/*.info.gz; do
 	echo -n "--> Installing $F ..."
 	/usr/bin/install-info --info-file=$F --dir-file=$INFODIR/dir
 	echo " done"
