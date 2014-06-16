665d1deb6d69412c3b5e8aab92ab79139c01f9fb
Ray Donnelly <mingw.android@gmail.com>
2014-06-17 00:50:48 +0100

    msys2-base: add shortcuts to installer, attempt to add icons (failed)

--- msys2-base/qt-ifw/packages/com.msys2.root.base/meta/installscript.js
+++ msys2-base/qt-ifw/packages/com.msys2.root.base/meta/installscript.js
@@ -8,3 +8,34 @@ Component.prototype.isDefault = function()
     // select the component by default
     return true;
 }
+
+function createShortcuts()
+{
+    var windir = installer.environmentVariable("WINDIR");
+    if (windir === "") {
+        QMessageBox["warning"]( "Error" , "Error", "Could not find windows installation directory");
+        return;
+    }
+
+    var cmdLocation = windir + "\\system32\\cmd.exe";
+    component.addOperation( "CreateShortcut",
+                            cmdLocation,
+                            "@StartMenuDir@/MSYS2 Shell.lnk",
+                            "/A /Q /K " + installer.value("TargetDir") + "\\msys2_shell.bat");
+
+    component.addOperation( "CreateShortcut",
+                            cmdLocation,
+                            "@StartMenuDir@/MinGW-w64 Win32 Shell.lnk",
+                            "/A /Q /K " + installer.value("TargetDir") + "\\mingw32_shell.bat");
+
+    component.addOperation( "CreateShortcut",
+                            cmdLocation,
+                            "@StartMenuDir@/MinGW-w64 Win64 Shell.lnk",
+                            "/A /Q /K " + installer.value("TargetDir") + "\\mingw64_shell.bat");
+}
+
+Component.prototype.createOperations = function()
+{
+    component.createOperations();
+    createShortcuts();
+}
