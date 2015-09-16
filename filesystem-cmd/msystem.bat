ed54eccce9716b9471c63bc1738ebbab637ed552
Stefan Zimmermann <zimmermann.code@gmail.com>
2015-09-16 08:50:10 +0200

    filesystem-cmd: improved environment management in msystem.bat

--- filesystem-cmd/msystem.bat
+++ filesystem-cmd/msystem.bat
@@ -18,9 +18,11 @@ if "%~1" == "/?" (
     echo In MSYS mode, an additional path containing wrapper scripts
     echo for BASH and PACMAN is prepended to %%PATH%%.
     echo.
-    echo The root directory of the MSYS2 installation
+    echo To work properly, this script must be located
+    echo in the cmd\ sub-directory of the MSYS2 installation,
+    echo or the root directory of the MSYS2 installation
     echo ^(usually C:\path\to\msys32 or ...\msys64^)
-    echo must be defined in %%MSYS2_ROOT%% or must be in %%PATH%%.
+    echo must be defined in %%MSYS2_ROOT%%.
     echo.
     echo After successful activation,
     echo the environment name will be stored in %%MSYS2_SYSTEM%%
@@ -50,6 +52,10 @@ if "%~1" == "/?" (
     echo MSYSTEM will try to automatically find it.
     exit /b 0
 )
+
+REM --------------------------------------------------------------------------
+REM No args
+
 if "%~1" == "" (
     REM only show info and exit
     if "%MSYS2_SYSTEM%" == "" (
@@ -61,16 +67,25 @@ if "%~1" == "" (
 )
 
 REM --------------------------------------------------------------------------
-REM Handle flags
+REM Handle install flag
 
-if /i "%~1" == "/D" (
-    REM deactivate MSYS2 environment
-    set MSYS2_SYSTEM=
-    goto prepare
-)
 if /i "%~1" == "/I" (
     REM install MSYS2 features into CMD shell extension frameworks
     goto install
+)
+
+
+REM ==========================================================================
+REM Environment management
+
+setlocal EnableDelayedExpansion
+
+REM --------------------------------------------------------------------------
+REM Handle environment deactivation flag
+
+if /i "%~1" == "/D" (
+    set MSYS2_SYSTEM=
+    goto deactivate
 )
 
 REM --------------------------------------------------------------------------
@@ -83,69 +98,79 @@ for %%M in (MSYS MINGW32 MINGW64) do if /i "%~1" == "%%M" (
 echo Invalid argument '%~1'. Call MSYSTEM /? for help.
 exit /b 1
 
-
 REM --------------------------------------------------------------------------
 REM Check system and create variables for MSYS2 (de)activation
 :prepare
 
-if "%MSYS2_ROOT%" == "" (
-    REM try to find MSYS2 root directory in %PATH%
-    for %%S in (msys2_shell.bat) do (
-        set "MSYS2_ROOT=%%~dp$PATH:S"
-        REM %MSYS2_ROOT% would not be set in current context w/o goto
-        REM (that often discussed strange set-in-for-loop behavior...)
-        goto checkRoot
-    )
-    :checkRoot
-    if "%MSYS2_ROOT%" == "" (
-        echo Can't determine MSYS2 root directory.
-        echo No %%MSYS2_ROOT%% defined and not in %%PATH%%.
-        set MSYS2_SYSTEM=
-        exit /b 1
-    )
-    REM remove trailing \
-    set "MSYS2_ROOT=%MSYS2_ROOT:~0,-1%"
+if not "%MSYS2_ROOT%" == "" (
+    goto customRoot
 )
-if not exist "%MSYS2_ROOT%\" (
-    echo %%MSYS2_ROOT%% '%MSYS2_ROOT%' does not exist or is not a directory.
-    set MSYS2_SYSTEM=
+
+REM no %MSYS2_ROOT% ==> assume that this script is in <MSYS2 root>\cmd\
+set "cmdRoot=%~dp0"
+REM remove trailing \ and cmd subdir
+for %%D in ("%cmdRoot:~0,-1%") do (
+    REM will not be exported on endlocal
+    set "MSYS2_ROOT=%%~dpD"
+)
+REM remove trailing \
+set "MSYS2_ROOT=%MSYS2_ROOT:~0,-1%"
+if not exist "%MSYS2_ROOT%\msys2_shell.bat" (
+    echo This script is not properly installed.
+    echo Should be in ^<MSYS2 root^>\cmd\
     exit /b 1
 )
+goto deactivate
 
-REM construct MSYS2 and MINGW32/64 bin paths
-set "MSYS2_PATH=%MSYS2_ROOT%\usr\local\bin;%MSYS2_ROOT%\usr\bin;%MSYS2_ROOT%\bin"
-set "MSYS2_MINGW32_PATH=%MSYS2_ROOT%\mingw32\bin"
-set "MSYS2_MINGW64_PATH=%MSYS2_ROOT%\mingw64\bin"
+:customRoot
 
+if not exist "%MSYS2_ROOT%\" (
+    echo %%MSYS2_ROOT%% '%MSYS2_ROOT%' does not exist or is not a directory.
+    exit /b 1
+)
 
 REM --------------------------------------------------------------------------
 REM Always deactivate current MSYS2 environment before activating a new one
 :deactivate
 
 REM remove any existing MSYS2 and MINGW32/64 bin paths from %PATH%
-call set "PATH=%%PATH:%MSYS2_PATH%;=%%"
-call set "PATH=%%PATH:%MSYS2_MINGW32_PATH%;=%%"
-call set "PATH=%%PATH:%MSYS2_MINGW64_PATH%;=%%"
+if not "%MSYS2_PATH%" == "" (
+    call set "PATH=%%PATH:%MSYS2_PATH%;=%%"
+)
+if not "%MSYS2_MINGW32_PATH%" == "" (
+    call set "PATH=%%PATH:%MSYS2_MINGW32_PATH%;=%%"
+)
+if not "%MSYS2_MINGW64_PATH%" == "" (
+    call set "PATH=%%PATH:%MSYS2_MINGW64_PATH%;=%%"
+)
 REM and also remove the extra MSYS tool wrapper scripts path
 call set "PATH=%%PATH:%~dp0msys;=%%"
 
 REM remove any <MSYS> or <MINGW32/64> tags from %PROMPT%
-REM (use temporary %_msysPrompt% to avoid too many %PROMPT% changes,
+REM (use temporary %_cleanPrompt% to avoid too many %PROMPT% changes,
 REM  which can result in strange shell behavior)
-set _cleanPrompt=%PROMPT:$LMSYS$G$S=%
-set _cleanPrompt=%_cleanPrompt:$LMINGW32$G$S=%
-set _cleanPrompt=%_cleanPrompt:$LMINGW64$G$S=%
+set "_cleanPrompt=%PROMPT:$LMSYS$G$S=%"
+set "_cleanPrompt=%_cleanPrompt:$LMINGW32$G$S=%"
+set "_cleanPrompt=%_cleanPrompt:$LMINGW64$G$S=%"
 
 REM check if only deactivation requested (via MSYSTEM /D)
 if "%MSYS2_SYSTEM%" == "" (
-    prompt %_cleanPrompt%
-    set _cleanPrompt=
-    exit /b 0
-)
+    set "_prompt=%_cleanPrompt%"
 
+    set MSYS2_PATH=
+    set MSYS2_MINGW32_PATH=
+    set MSYS2_MINGW64_PATH=
+
+    goto end
+)
 
 REM --------------------------------------------------------------------------
 :activate
+
+REM construct MSYS2 and MINGW32/64 bin paths
+set "MSYS2_PATH=%MSYS2_ROOT%\usr\local\bin;%MSYS2_ROOT%\usr\bin;%MSYS2_ROOT%\bin"
+set "MSYS2_MINGW32_PATH=%MSYS2_ROOT%\mingw32\bin"
+set "MSYS2_MINGW64_PATH=%MSYS2_ROOT%\mingw64\bin"
 
 REM prepend MSYS2 bin paths and/or MINGW32/64 bin paths to %PATH%
 if "%MSYS2_SYSTEM%" == "MSYS" (
@@ -159,14 +184,24 @@ if "%MSYS2_SYSTEM%" == "MINGW64" (
     set "PATH=%MSYS2_MINGW64_PATH%;%PATH%"
 )
 
-REM prepend the appropriate <MSYS> or <MINGW32/64> tag to %PROMPT%
-prompt $L%MSYS2_SYSTEM%$G$S%_cleanPrompt%
-set _cleanPrompt=
+REM prepend the appropriate <MSYS> or <MINGW32/64> tag to prompt
+set "_prompt=$L%MSYS2_SYSTEM%$G$S%_cleanPrompt%"
 
+REM --------------------------------------------------------------------------
+REM Successfully finished MSYS2 environment (de)activation
+:end
+
+REM export new environment variables
+endlocal & set "MSYS2_SYSTEM=%MSYS2_SYSTEM%" ^
+         & set "MSYS2_PATH=%MSYS2_PATH%" ^
+         & set "MSYS2_MINGW32_PATH=%MSYS2_MINGW32_PATH%" ^
+         & set "MSYS2_MINGW64_PATH=%MSYS2_MINGW64_PATH%" ^
+         & set "PATH=%PATH%" ^
+         & set "PROMPT=%_prompt%"
 exit /b 0
 
 
-REM --------------------------------------------------------------------------
+REM ==========================================================================
 REM Install MSYS2 features into CMD shell extension frameworks
 :install
 
@@ -183,9 +218,9 @@ REM Install MSYS2 auto-completion features into CLINK
 
 setlocal EnableDelayedExpansion
 
-if /i "%~3" == "" (
+if "%~3" == "" (
     REM no clink settings dir given ==> try to find
-    set "clinkDir=%localappdata%\clink"
+    set "clinkDir=%LOCALAPPDATA%\clink"
     if not exist "!clinkDir!\" (
         echo Could not find CLINK settings directory.
         echo Please provide it as additional argument.
@@ -224,3 +259,4 @@ echo Writing '%clinkDir%\msys2.lua'
 echo !msysLua! > %clinkDir%\msys2.lua
 
 endlocal
+exit /b 0
