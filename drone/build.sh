3d7ae6ef47db0f9741511853ba3ed04c08c84985
Qian Hong <qhong@codeweavers.com>
2016-02-04 23:59:27 +0800

    drone: Use `git show -m` to show merged commit, fix pull request support.

--- drone/build.sh
+++ drone/build.sh
@@ -6,7 +6,7 @@ git config --global user.email "ci@msys2.org"
 git config --global user.name "MSYS2 Build Bot"
 
 # fetch first changed file, assume at most one package touched per commit
-TOUCHED=`git show --pretty="format:" --name-only | grep . | head -1`
+TOUCHED=`git show -m --pretty="format:" --name-only | grep . | head -1`
 PKGDIR=`dirname $TOUCHED`
 if [ "$PKGDIR" = "." ]
 then
