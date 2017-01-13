6741d1ac0dd5d9e2db9424893a2eb04dc04405d5
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-01-13 12:13:57 +0100

    msys2-runtime: add helper script to update the patch set
    It is a bit tedious, in particular with the recent switch from a Git
    branch in https://github.com/Alexpux/Cygwin to a patch set, to update
    the msys2-runtime.
    
    Let's simplify things again by scripting the process that turns real Git
    commits into that patch set.
    
    It is still a bit cumbersome to keep track of the MSYS2 runtime patches
    since the msys2-master branch is always rebased, with no record of prior
    iterations, of course.
    
    This script has been contributed in almost the same shape as part of
    https://github.com/Alexpux/MSYS2-packages/pull/786, with one modification:
    the current version handles Git for Windows' merging rebases [*1*]
    gracefully.
    
    Footnote *1*: "Merging rebases" are Git for Windows' strategy to rebase
    *and* retain the previous commit history. For details, please see
    https://github.com/git-for-windows/build-extra/blob/4cacaabc1/shears.sh#L12-L18
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ msys2-runtime/update-patches.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+die () {
+	echo "$*" >&2
+	exit 1
+}
+
+cd "$(dirname "$0")" ||
+die "Could not cd to msys2-runtime/"
+
+base_tag=refs/tags/cygwin-"$(sed -ne 'y/./_/' -e 's/^pkgver=//p' <PKGBUILD)"-release
+
+git rev-parse --verify HEAD >/dev/null &&
+git update-index -q --ignore-submodules --refresh &&
+git diff-files --quiet --ignore-submodules &&
+git diff-index --cached --quiet --ignore-submodules HEAD -- ||
+die "Clean worktree required"
+
+git rm 0*.patch ||
+die "Could not remove previous patches"
+
+git -c core.abbrev=7 -C src/msys2-runtime format-patch -o ../.. --signature=2.9.0 \
+	$base_tag.. ^HEAD^{/Start.the.merging.rebase} ||
+die "Could not generate new patch set"
+
+patches="$(ls 0*.patch)" &&
+git add $patches ||
+die "Could not stage new patch set"
+
+in_sources="$(echo "$patches" | sed "{s/^/        /;:1;N;s/\\n/\\\\n        /;b1}")"
+in_prepare="$(echo "$patches" |
+	sed "{s|^|  git am --committer-date-is-author-date \"\${srcdir}\"/|;:1;N;
+	 s|\\n|\\\\n  git am --committer-date-is-author-date \"\${srcdir}\"/|;b1}")"
+sed -i -e "/^        0.*\.patch$/{:1;N;/[^)]$/b1;s|.*|$in_sources)|}" \
+	-e "/^  git am.*\.patch$/{:2;N;/[^}]$/b2;s|.*|$in_prepare\\n\\}|}" \
+	PKGBUILD ||
+die "Could not update the patch set in PKGBUILD"
+
+updpkgsums ||
+die "Could not update the patch set checksums in PKGBUILD"
+
+git add PKGBUILD ||
+die "Could not stage updates in PKGBUILD"
