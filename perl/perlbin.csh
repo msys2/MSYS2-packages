a832188b01a583c8ec2c8bc3543872334aa9dd30
Alexey Pavlov <alexpux@gmail.com>
2017-02-16 14:17:31 +0300

    Don't set PERL_PATH in sturtup scripts. Synchronize with Arch Linux.

--- perl/perlbin.csh
+++ perl/perlbin.csh
@@ -1,5 +1,5 @@
 # Set path to perl scriptdirs if they exist
-# https://wiki.archlinux.org/index.php/Perl_Policy#Binaries_and_Scripts
+# https://wiki.archlinux.org/index.php/Perl_Policy#Binaries_and_scripts
 # Added /usr/bin/*_perl dirs for scripts
 # Remove /usr/lib/perl5/*_perl/bin in next release
 
--- perl/perlbin.sh
+++ perl/perlbin.sh
@@ -1,22 +1,17 @@
 # Set path to perl scriptdirs if they exist
-# https://wiki.archlinux.org/index.php/Perl_Policy#Binaries_and_Scripts
+# https://wiki.archlinux.org/index.php/Perl_Policy#Binaries_and_scripts
 # Added /usr/bin/*_perl dirs for scripts
 # Remove /usr/lib/perl5/*_perl/bin in next release
 
-if test -z "${PERL_PATH}"; then
+[ -d /usr/bin/site_perl ] && PATH=$PATH:/usr/bin/site_perl
+[ -d /usr/lib/perl5/site_perl/bin ] && PATH=$PATH:/usr/lib/perl5/site_perl/bin
 
-    [ -d /usr/bin/site_perl ] && PERL_PATH="${PERL_PATH:+${PERL_PATH}:}/usr/bin/site_perl"
-    [ -d /usr/lib/perl5/site_perl/bin ] && PERL_PATH="${PERL_PATH:+${PERL_PATH}:}/usr/lib/perl5/site_perl/bin"
+[ -d /usr/bin/vendor_perl ] && PATH=$PATH:/usr/bin/vendor_perl
+[ -d /usr/lib/perl5/vendor_perl/bin ] && PATH=$PATH:/usr/lib/perl5/vendor_perl/bin
 
-    [ -d /usr/bin/vendor_perl ] && PERL_PATH="${PERL_PATH:+${PERL_PATH}:}/usr/bin/vendor_perl"
-    [ -d /usr/lib/perl5/vendor_perl/bin ] && PERL_PATH="${PERL_PATH:+${PERL_PATH}:}/usr/lib/perl5/vendor_perl/bin"
+[ -d /usr/bin/core_perl ] && PATH=$PATH:/usr/bin/core_perl
 
-    [ -d /usr/bin/core_perl ] && PERL_PATH="${PERL_PATH:+${PERL_PATH}:}/usr/bin/core_perl"
-
-fi
-
-export PERL_PATH PATH="${PATH}:${PERL_PATH}"
+export PATH
 
 # If you have modules in non-standard directories you can add them here.
 #export PERLLIB=dir1:dir2
-
