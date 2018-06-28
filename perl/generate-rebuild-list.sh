0687a044230c1ce86395a4630192d30c9c607f69
Alexey Pavlov <alexpux@gmail.com>
2018-06-28 11:41:32 +0300

    perl: Update to 5.28.0

--- perl/generate-rebuild-list.sh
+++ perl/generate-rebuild-list.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-pkgfile -r "^/usr/lib/perl5/vendor_perl/auto/.*\.so$" | sed 's#^.*/##' | sort -u
-ssh celestia PATH=/usr/local/bin:\$PATH\; /home/bluewind/bin/sogrep-all libperl.so
+pkgfile -r "^/usr/lib/perl5/vendor_perl/auto/.*\.dll$" | sed 's#^.*/##' | sort -u
