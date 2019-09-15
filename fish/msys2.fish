93657eb12914e000a51caa622690543e88cb9ec3
Viktor Szakats <commit@vszakats.net>
2019-09-15 15:35:04 +0000

    use HTTPS in URLs where supported

--- fish/msys2.fish
+++ fish/msys2.fish
@@ -3,7 +3,7 @@
 # public domain worldwide. This software is distributed without any warranty. 
 # You should have received a copy of the CC0 Public Domain Dedication along 
 # with this software. 
-# If not, see <http://creativecommons.org/publicdomain/zero/1.0/>. 
+# If not, see <https://creativecommons.org/publicdomain/zero/1.0/>. 
 
 
 # System-wide profile file
