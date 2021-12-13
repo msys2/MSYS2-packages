7fba1e9de4662a49395e60ca6bfad63af3ec977a
Christoph Reiter <reiter.christoph@gmail.com>
2021-12-13 22:51:11 +0100

    msys2-devel is gone

--- .ci/ci-build.sh
+++ .ci/ci-build.sh
@@ -40,7 +40,7 @@ for package in "${packages[@]}"; do
     echo "::group::[build] ${package}"
     execute 'Fetch keys' "$DIR/fetch-validpgpkeys.sh"
     # Ensure the toolchain is installed before building the package
-    execute 'Installing the toolchain' pacman -S --needed --noconfirm --noprogressbar base-devel msys2-devel
+    execute 'Installing the toolchain' pacman -S --needed --noconfirm --noprogressbar base-devel
     execute 'Building binary' makepkg --noconfirm --noprogressbar --nocheck --syncdeps --rmdeps --cleanbuild
     execute 'Building source' makepkg --noconfirm --noprogressbar --allsource
     echo "::endgroup::"
