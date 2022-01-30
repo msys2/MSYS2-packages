b34d3118197f388d3c35a76434766d61d2ffb96a
Christoph Reiter <reiter.christoph@gmail.com>
2022-01-30 12:11:12 +0100

    filesystem: warn users that Windows 7/8 support is going away
    This will be shown on a login shell, so the first two lines when
    users open a MSYS2 terminal.

--- filesystem/profile.000-msys2.sh
+++ filesystem/profile.000-msys2.sh
@@ -4,3 +4,21 @@ if [ ! "${MINGW_PREFIX}" = "" ]; then
     XDG_DATA_DIRS="/usr/local/share/:/usr/share/"
     export XDG_DATA_DIRS="$MINGW_PREFIX/share/:$XDG_DATA_DIRS"
 fi
+
+# Warn the user on the first login shell in case we detect a too old Windows version
+function _warn_deprecated_winver()
+{
+    if [ "$__MSYS2_WINDOWS_VERSION_WARNING_DONE" = "true" ]; then
+        return;
+    fi
+    local winver;
+    winver="$(uname -s | sed -ne 's/\([^-]*\)-\([^-]*\).*/\2/p')"
+    if [ "$winver" = "6.1" ] || [ "$winver" = "6.2" ]; then
+        export __MSYS2_WINDOWS_VERSION_WARNING_DONE="true"
+        echo -e "\e[1;33mThe MSYS2 project is planning to drop active support of Windows 7 and 8.0 sometime during 2022.\e[1;0m" 1>&2;
+        echo -e "\e[1;33mFor more information visit https://www.msys2.org/docs/windows_support\e[1;0m" 1>&2;
+    fi
+}
+
+_warn_deprecated_winver;
+unset _warn_deprecated_winver;
